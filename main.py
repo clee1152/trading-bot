4a04e5ee323776bf0327c2dded993b207c869c76
ahhlmao <chrislee011502@gmail.com>
2022-01-12 23:06:38 -0800

    Added ease of use functionality to main.py.

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import strategies as strats
 import trading_bot as tb
 
 # Prompt user for credentials and number of stocks from the S&P 500.
-key = input("Enter Key: ")
-secret = input("Enter Secret Key: ")
+credentials = open("alpacainfo.txt", "r")
+line1 = credentials.readline()
+line2 = credentials.readline() 
+credentials.close()
+
+if line1 == line2:
+    key = input("Enter Key: ")
+    secret = input("Enter Secret Key: ")
+else:
+    key = line1.rstrip("\n")
+    secret = line2.rstrip("\n")
+
 number = int(input("Enter Number of Stocks: "))
 bp = int(input("Enter Buying Power: "))
 
